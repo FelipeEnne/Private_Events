72f33bbb31316194b1c5d0935f0a1b0a78da29b8
Felipe Enne Mendes Ribeiro <felipeenne@gmail.com>
2020-07-13 12:25:55 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   get '/signup', to: 'users#new'
-  get '/', to: 'users#show'
+  get '/dashboard', to: 'users#show'
   resources :users, except: [:new, :show]
   resources :events
 
